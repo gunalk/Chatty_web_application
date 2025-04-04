e94b4ed6031890a511774c38b733e6ca4f0d15e5
Gunal <lfgunal060@gail.com>
2025-04-05 00:28:00 +0530

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 import path from "path";
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
