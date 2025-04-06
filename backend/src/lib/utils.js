7a64ec67d9c2564638e3cc5f5315d5401a29d292
Gunal <lfgunal060@gail.com>
2025-04-06 16:58:37 +0530

    secure changed

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: true,
-    secure: process.env.NODE_ENV != "development",
+    secure: false,
   });
 
   return token;
