5ce4d5236ed85de37f2d81b3fe66953b8c84ee6d
Gunal <lfgunal060@gail.com>
2025-04-06 15:57:08 +0530

    fixed some code

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,13 +26,7 @@ const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 server.listen(PORT || 5000, () => {
   console.log("server is listening on port " + PORT);
   connectDB();
